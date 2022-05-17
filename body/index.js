1cc4f53b58ef0657036fa43452729692992d0307
Thiago <thiago.augusto@impactaweb.com.br>
2022-05-17 15:32:55 -0300

    bot ficando online

--- body/index.js
+++ body/index.js
@@ -1,19 +1,17 @@
-const express = request('express');
-const app = express();
+// chamadas do discord.js
+const { Client, Intents } = require("discord.js");
+const { config } = require("./config.json");
+const dotenv = require('dotenv');
 
-app.get("/", (request, response) => {
-    const ping = new Date();
-    ping.setHours(ping.setHours() - 3);
-    console.log(`Date ${ping.getUTCHours()}:${ping.getUTCMinutes()}:${ping.getUTCSeconds()}`);
-    response.sendStatus(200);
+dotenv.config();
+
+//criar um novo cliente
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+//debug
+client.once('ready', () => {
+    console.log('Estou funcionando');
 });
 
-app.listen(process.env.PORT); // Recebe a solicitação
-
-const Discord = required("discord.js") // biblioteca do discord.js
-
-const client = new Discord.Client(); // criar um novo cliente
-
-const config = require("./config.json") // pegando o prefixo do bot parar respostas de comandos
-
-client.login(process.env.TOKEN); //Ligar o bot caso acesse o token
+//fazer o bot logar
+client.login(process.env.TOKEN);
